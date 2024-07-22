cc31b4c2f34c231ec7fcfe5d2d1ae58779be83ab
Zach Cohn <zcohn@wustl.edu>
2024-07-22 12:27:08 -0500

    added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,49 +1,49 @@
-"use client"
+"use client";
 
-import './globals.css'
-import './theming.css'
-import "../../public/fonts/fonts.css"
-import type {Metadata} from 'next'
-import Navbar from "../components/NavBar/NavBar"
-import React, {useState} from "react";
+import "./globals.css";
+import "./theming.css";
+import "../../public/fonts/fonts.css";
+import type { Metadata } from "next";
+import Navbar from "../components/NavBar/NavBar";
+import React, { useState } from "react";
 import MainContainer from "@/components/Layout/MainContainer/MainContainer";
-import "../bootstrap.min.css"
+import "../bootstrap.min.css";
 import Footer from "@/components/Footer/Footer";
-
-// TODO: find a way to use metadata and the theme switcher
-//  the issue is that metadata requires server side component and the theme switcher requires client side component
-// export const metadata: Metadata = {
-//   title: 'Zach Cohn',
-//   description: 'Zach Cohn is a software engineer and theatre artist based in New York City.',
-// }
-
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export default function RootLayout({
-                                       children,
-                                   }: {
-    children: React.ReactNode
+  children,
+}: {
+  children: React.ReactNode;
 }) {
-
-
-    return (
-        <html lang="en">
-        <head>
-            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-            <link rel="manifest" href="/site.webmanifest"/>
-            <meta name="viewport" content="width=device-width,initial-scale=1" />
-        </head>
-        <body>
-        <MainContainer>
-            {children}
-
-
-        </MainContainer>
-        <Footer/>
-
-        </body>
-        </html>
-    )
+  return (
+    <html lang="en">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+      </head>
+      <body>
+        <MainContainer>{children}</MainContainer>
+        <Footer />
+        <GoogleAnalytics gaId="G-PPDRH7FT49" />
+      </body>
+    </html>
+  );
 }
-
