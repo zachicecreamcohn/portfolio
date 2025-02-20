3c5d29415881cabac2b3e9c4eaa49ce017d6060d
Zach Cohn <zcohn@wustl.edu>
2025-02-20 01:30:36 -0600

    resume url fix and small copy changes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,46 +5,51 @@ import TopContainer from "@/components/Layout/TopContainer/TopContainer";
 import Image from "next/image";
 import Link from "next/link";
 import NavBar from "@/components/NavBar/NavBar";
-import Footer from "@/components/Footer/Footer";
 
 export default function About() {
+  return (
+    <>
+      <NavBar />
+      <TopContainer>
+        <div className={styles.container + " container"}>
+          <div className={styles.left}>
+            <div className={styles.imageContainer}>
+              <Image
+                src={"/images/headshot.png"}
+                alt={"Zach Cohn"}
+                layout="responsive"
+                width={400}
+                height={400}
+              />
+            </div>
+          </div>
+          {/*<div className="d-none d-sm-block col-1  col-lg-2"></div>*/}
+          <div className={styles.right + " col-12 col-sm-7 col-lg-6"}>
+            <div className={styles.aboutContainer}>
+              <h1>hello!</h1>
 
-    return (
-        <>
-            <NavBar/>
-            <TopContainer>
-                <div className={styles.container + " container"}>
-
-                        <div className={styles.left}>
-                            <div className={styles.imageContainer}>
-                                <Image
-                                    src={"/images/headshot.png"}
-                                    alt={"Zach Cohn"}
-                                    layout="responsive"
-                                    width={400}
-                                    height={400}
-                                />
-                            </div>
-                        </div>
-                        {/*<div className="d-none d-sm-block col-1  col-lg-2"></div>*/}
-                        <div className={styles.right + " col-12 col-sm-7 col-lg-6"}>
-                            <div className={styles.aboutContainer}>
-                                <h1>hello!</h1>
-
-                                <p>My name is Zach Cohn, and I&apos;m a developer and theatre maker from NYC.</p>
-                                <p>I&apos;m currently studying Drama and Computer Science at Washington University in
-                                    St.
-                                    Louis.</p>
-                                <p>I love to explore the intersection of technology and art, especially projection
-                                    mapping,
-                                    theatrical lighting, and performance automation. I&apos;m also an avid cyclist.</p>
-                                <div className={styles.cvContainer}>
-                                    <Link href={"/cv"} className={"animate-hover"}>Resume</Link>
-                                </div>
-                            </div>
-                        </div>
-                </div>
-            </TopContainer>
-        </>
-    )
+              <p>
+                My name is Zach Cohn, and I&apos;m a developer and theatre maker
+                from NYC.
+              </p>
+              <p>
+                I&apos;m currently studying Drama and Computer Science at
+                Washington University in St. Louis.
+              </p>
+              <p>
+                I love to explore the intersection of technology and art,
+                especially projection mapping, theatrical lighting, and
+                performance automation.
+              </p>
+              <div className={styles.cvContainer}>
+                <Link href={"/resume"} className={"animate-hover"}>
+                  Resume
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </TopContainer>
+    </>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,72 +1,73 @@
-"use client"
+"use client";
 
 import Button from "@/components/Button/Button";
 import TopContainer from "@/components/Layout/TopContainer/TopContainer";
 import styles from "./Home.module.css";
 import Image from "next/image";
 import Link from "next/link";
-import {Code, Brush} from 'tabler-icons-react';
+import { Code } from "tabler-icons-react";
 import React from "react";
 import NavBar from "@/components/NavBar/NavBar";
 
 export default function Home() {
+  return (
+    <>
+      <NavBar />
 
-    return (
-        <>
-            <NavBar/>
+      <TopContainer>
+        <div className={styles.heroContainer}>
+          <div className={`col-xs-12 col-md-8 ${styles.left}`}>
+            <div className={styles.hero}>
+              <h1>
+                im <span className={styles.name}>zach cohn</span>,
+              </h1>
+              <h1>
+                <span className={styles.color}>dev</span> and{" "}
+                <span className={styles.color}>theatre artist</span>
+              </h1>
+              <p>
+                I do theatre, software, and sometimes both at the same time!
+              </p>
 
-            <TopContainer>
-                <div className={styles.heroContainer}>
-                    <div className={`col-xs-12 col-md-8 ${styles.left}`}>
-                        <div className={styles.hero}>
-                            <h1>im <span className={styles.name}>zach cohn</span>,</h1>
-                            <h1>
-                                <span className={styles.color}>dev</span> and <span className={styles.color}>theatre artist</span>
-                            </h1>
-                            <p>I&apos;m passionate about building full-stack, impactful applications and the ways
-                                emerging technologies interface with the arts.</p>
-
-                            <div className={styles.buttonContainer}>
-                                <Link href={"/contact"} className={styles.contactButton}>
-                                    <Button style="primary" onClick={() => {
-                                    }}>Contact Me</Button>
-                                </Link>
-                                <div>
-                                <Link href={"/dev"}>
-                                    <Button style="secondary" onClick={() => {
-                                    }}>
-                                        <Code size={24} strokeWidth={1.5} color={"#000000"}/>
-                                    </Button>
-
-                                </Link>
-                                <Link href={"/design"}>
-                                    <Button style="secondary" onClick={() => {
-                                    }}>
-                                        <Image
-                                            src={"/icons/stage-lights.svg"}
-                                            alt={"Theater and other work"}
-                                            width={24}
-                                            height={24}
-                                            className={styles['theater-icon']}
-                                        />
-                                    </Button>
-                                </Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className={`col-md-4 ${styles.right}`}>
-                        <Image src={"/images/hero.png"} alt={"png artwork of Zach Cohn smiling"}
-                               className={styles.heroImage}
-                               width={400}
-                               height={400}
-                        />
-                    </div>
+              <div className={styles.buttonContainer}>
+                <Link href={"/contact"} className={styles.contactButton}>
+                  <Button style="primary" onClick={() => {}}>
+                    Contact Me
+                  </Button>
+                </Link>
+                <div>
+                  <Link href={"/dev"}>
+                    <Button style="secondary" onClick={() => {}}>
+                      <Code size={24} strokeWidth={1.5} color={"#000000"} />
+                    </Button>
+                  </Link>
+                  <Link href={"/design"}>
+                    <Button style="secondary" onClick={() => {}}>
+                      <Image
+                        src={"/icons/stage-lights.svg"}
+                        alt={"Theater and other work"}
+                        width={24}
+                        height={24}
+                        className={styles["theater-icon"]}
+                      />
+                    </Button>
+                  </Link>
                 </div>
+              </div>
+            </div>
+          </div>
 
-
-            </TopContainer>
-        </>
-    )
+          <div className={`col-md-4 ${styles.right}`}>
+            <Image
+              src={"/images/hero.png"}
+              alt={"png artwork of Zach Cohn smiling"}
+              className={styles.heroImage}
+              width={400}
+              height={400}
+            />
+          </div>
+        </div>
+      </TopContainer>
+    </>
+  );
 }
