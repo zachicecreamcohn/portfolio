0e3867cd6d0a4751dcfd3b62580f971b744f5a1b
Zach Cohn <software@zachwcohn.com>
2025-12-20 19:58:42 -0500

    add resume redirect route

--- next.config.js
+++ next.config.js
@@ -1,13 +1,23 @@
 // @ts-ignore
-const withFonts = require('next-fonts');
+const withFonts = require("next-fonts");
 
 const nextConfig = withFonts({
-    webpack(config, options) {
-        return config;
-    },
-    images: {
-        domains: ['localhost', 'https://zachcohn.sfo3.digitaloceanspaces.com'],
-    }
+  webpack(config, options) {
+    return config;
+  },
+  images: {
+    domains: ["localhost", "https://zachcohn.sfo3.digitaloceanspaces.com"],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/resume/lighting-and-video",
+        destination:
+          "https://docs.google.com/document/d/e/2PACX-1vR42hKiBPp0BEwj27hHM8mVaju2NBCkGMRzjlBMv5nAK3w19reS3RSg4J-OfRFyzJkpwz3RcLWFKthS/pub",
+        permanent: false,
+      },
+    ];
+  },
 });
 
 module.exports = nextConfig;
